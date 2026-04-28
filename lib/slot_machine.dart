c57c9c5dd954758ec67b70b228762966f2d6f69a
vfedotova418-png <vfedotova418@gmail.com>
2026-04-28 11:40:30 +0300

    Update SoundService to use audioplayers for Web and Android

--- lib/slot_machine.dart
+++ lib/slot_machine.dart
@@ -26,7 +26,6 @@ class _SlotMachineState
   var _message = '';
   var _isSpinning = false;
   var _isMuted = false;
-  var _backgroundStarted = false;
 
   Future<String> _spinReel({
     required int totalTicks,
@@ -54,15 +53,11 @@ class _SlotMachineState
 
   Future<void> _spin() async {
     if (_coins <= 0 || _isSpinning) return;
-    SoundService.playClick();
+    await SoundService.playClick();
     setState(() {
       _isSpinning = true;
       _message = '';
     });
-    if (!_backgroundStarted) {
-      SoundService.playBackground();
-      _backgroundStarted = true;
-    }
     final result1 = await _spinReel(
       totalTicks: 10,
       onTick: (val) =>
@@ -82,26 +77,30 @@ class _SlotMachineState
     await Future.delayed(
       Duration(milliseconds: 300),
     );
+    String newMessage;
+    int coinsChange;
+    if (result1 == result2 &&
+        result2 == result3) {
+      if (result1 == 'assets/images/seven.png') {
+        coinsChange = 10;
+        newMessage = 'ДЖЕКПОТ! 🎰🎰🎰 +10 монет';
+        await SoundService.playJackpot();
+      } else {
+        coinsChange = 3;
+        newMessage = 'Победа! 🎉 +3 монеты';
+        await SoundService.playWin();
+      }
+    } else {
+      coinsChange = -1;
+      newMessage =
+          'Попробуй ещё раз 😔 -1 монета';
+      await SoundService.playLose();
+    }
+
     setState(() {
       _isSpinning = false;
-      if (result1 == result2 &&
-          result2 == result3) {
-        if (result1 ==
-            'assets/images/seven.png') {
-          _coins += 10;
-          _message = 'ДЖЕКПОТ! 🎰🎰🎰 +10 монет';
-          SoundService.playJackpot();
-        } else {
-          _coins += 3;
-          _message = 'Победа! 🎉 +3 монеты';
-          SoundService.playWin();
-        }
-      } else {
-        _coins -= 1;
-        _message =
-            'Попробуй ещё раз 😔 -1 монета';
-        SoundService.playLose();
-      }
+      _coins += coinsChange;
+      _message = newMessage;
     });
   }
 
